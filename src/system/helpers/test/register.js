f7029dd4ace2ee4cc8cb6172f18fd69b1ec9837c
Glen Arrowsmith <glen.arrowsmith@gmail.com>
2015-01-16 14:50:06 +1000

    added usernames to users

--- src/system/helpers/test/register.js
+++ src/system/helpers/test/register.js
@@ -5,9 +5,11 @@ module.exports = function ( _request, _email, _password ) {
 
 	return function () {
 		var cb = _.last( arguments );
+		var username = _email.split( '@' )[ 0 ];
 		_request.post( config.baseUrl + '/api/user/register', {
 			body: {
 				email: _email,
+				username: username,
 				password: _password || 'password'
 			}
 		}, function ( _error, _res, _user ) {
