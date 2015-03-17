ba3bd0cf31a1b7f9252c876373a37d1f7900b18f
Glen Arrowsmith <glen.arrowsmith@gmail.com>
2015-03-18 09:35:51 +1000

    Added cors middleware

--- src/grailed/config/express.js
+++ src/grailed/config/express.js
@@ -87,6 +87,14 @@ module.exports = {
 							app.use( method || cookieParser() );
 							break;
 
+						case 'cors':
+							app.use( function ( req, res, next ) {
+								res.header( "Access-Control-Allow-Origin", "*" );
+								res.header( "Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept" );
+								next();
+							} );
+							break;
+
 						case 'i18n':
 							try {
 
