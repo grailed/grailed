92649f2b72396bc227002cfcb701c38198934096
David Tsuji <david@flodge.com>
2015-07-08 11:08:30 +1000

    addes client assets styles
    in addition to components we need a global space to store assets and styles

--- src/tasks/index.js
+++ src/tasks/index.js
@@ -1,14 +1,16 @@
 module.exports = function ( gulp ) {
 	global.gulp = gulp;
 
+	require( './assets' );
 	require( './components' );
 	require( './dependencies' );
 	require( './reload' );
 	require( './server' );
+	require( './styles' );
 	require( './watch' );
 
 	return {
-		'build': [ 'src/client/dependencies', 'src/client/components' ],
+		'build': [ 'src/client/dependencies', 'src/client/styles', 'src/client/assets', 'src/client/components' ],
 		'default': [ 'build', 'minify' ],
 		'minify': [ 'src/client/components/minify' ],
 		'run': [ 'default', 'server', 'watch' ],
